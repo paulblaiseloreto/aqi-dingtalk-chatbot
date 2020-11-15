eacb44834255441e85bf7440e52860ea121af998
Paul Blaise <paulloreto@gmail.com>
2020-11-16 06:36:32 +0800

    refactor and gitignore

--- lambda_function.py
+++ lambda_function.py
@@ -7,42 +7,8 @@ api_url = 'https://api.weatherbit.io/v2.0/current/airquality?city=bejing&key=cdb
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-    
-def getAQI():
-        response = requests.get(api_url)
-        posts = json.loads(response.text) #load data into a dict of objects, posts
-        aqi=posts['data'][0]['aqi'] #store AQI only
-        return aqi
-    
-# AQI Color
-def getColor(aqi):
-    if aqi <= 50:
-        return "Green"
-    elif aqi <= 100:
-        return "Yellow"
-    elif aqi <= 150:
-        return "Orange"
-    elif aqi <= 200:
-        return "Red"
-    elif aqi <= 300:
-        return "Purple"
-    else:
-        return "Maroon"
-    
-def post_markdown(title, text):
-    aqi = getAQI()
-    data = {
-        "msgtype": "markdown",
-        "markdown": {
-            "title": title,
-            "text": "Current AQI of Beijing is {}. You're {}".format(aqi, getColor(aqi))
-        }
-    }
-    post(data)
-
-# Send messages to the DingTalk group
 def post(data):
-    webhook_url='https://oapi.dingtalk.com/robot/send?access_token=46aa2f5571f947b73231c160dd8084771db63b23583c6539d44560ff14bed0f5' #the URL to the webhook of the DingTalk group chat bot
+    webhook_url='https://oapi.dingtalk.com/robot/send?access_token=46aa2f5571f947b73231c160dd8084771db63b23583c6539d44560ff14bed0f5' #the URL to the DingTalk group chatbot
     headers = {'Content-Type': 'application/json; charset=utf-8'}
     post_data = json.dumps(data)
     try:
@@ -62,8 +28,50 @@ def post(data):
             logger.error("Send Error:%s" % error_data)
             requests.post(webhook_url, headers=headers, data=json.dumps(error_data))
         return result
+ 
+# AQI Color
+def getColor(aqi):
+    if aqi <= 50:
+        return "Green"
+    elif aqi <= 100:
+        return "Yellow"
+    elif aqi <= 150:
+        return "Orange"
+    elif aqi <= 200:
+        return "Red"
+    elif aqi <= 300:
+        return "Purple"
+    else:
+        return "Maroon" 
+        
+def prepare_data(title, text, aqi):
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": title,
+            "text": "Current AQI of Beijing is {}. You're {}".format(aqi, getColor(aqi))
+        }
+    }
+    return data       
+        
+def callAPI():
+    response = requests.get(api_url)    
+
+    if (response.status_code == 200 or response.status_code == 201):
+        posts = json.loads(response.text) #load data into a dict of objects, posts
+        aqi=posts['data'][0]['aqi'] #store AQI only
+        return aqi
+    else:
+        return None       
         
 def lambda_handler(event, context):
-    post_markdown('title', 'text')
-    #print("Current AQI of Beijing is {}. You're {}".format(aqi, getColor(aqi)))
+    response = callAPI()
+
+    if response is not None:
+        data = prepare_data('title', 'text', response)
+        post(data)
+
+    else:
+        print('Failed Request')
+
     return 1
